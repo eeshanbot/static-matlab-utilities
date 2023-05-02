b8f5071913865ad1732e61f6cd59bcea0d722209
eesh meerkat <eeshan137@gmail.com>
2023-05-02 08:50:05 -0400

    lower font size

--- startup.m
+++ startup.m
@@ -13,7 +13,7 @@ fprintf('\n')
 %% figures
 set(0,'DefaultTextFontName','Lato')
 set(0,'DefaultAxesFontName','Lato')
-set(0,'DefaultAxesFontSize',18)
+set(0,'DefaultAxesFontSize',14)
 set(0,'DefaultAxesLineWidth',1)
 set(0, 'DefaultFigureColor', 'w');
 set(0, 'DefaultLineLineWidth', 2);
